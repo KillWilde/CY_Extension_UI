b0df9c3a99b7503513f7c059877f17f46c49a13d
Megatron <megatroncy@163.com>
2019-11-14 20:19:58 +0800

    增加sqlite3

---
+++ CY_Extension_UI/CY_Extension_UI/Manager/SqliteManager.h
@@ -0,0 +1,17 @@
+//
+//  SqliteManager.h
+//  CY_Extension_UI
+//
+//  Created by Megatron on 2019/11/14.
+//  Copyright © 2019 SaturdayNight. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SqliteManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CY_Extension_UI/CY_Extension_UI/Manager/SqliteManager.m
@@ -0,0 +1,16 @@
+//
+//  SqliteManager.m
+//  CY_Extension_UI
+//
+//  Created by Megatron on 2019/11/14.
+//  Copyright © 2019 SaturdayNight. All rights reserved.
+//
+
+#import "SqliteManager.h"
+#import <sqlite3.h>
+
+static sqlite3 *db = nil;
+
+@implementation SqliteManager
+
+@end
